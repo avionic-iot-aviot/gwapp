0a11656497354ddfd5b5290ceaf55e7f87c82fba
Carmelo <carmelo@Mac-mini-di-Deborah.local>
2020-03-04 17:36:14 +0100

    new fix

--- backend/src/resolv.ts
+++ backend/src/resolv.ts
@@ -6,12 +6,12 @@ import ResolvService from './services/resolvService';
 const resolvService = new ResolvService();
 const delay = require('delay');
  
-(async() => {
-    const result = await delay(60000);
+// (async() => {
+//     const result = await delay(60000);
 
-    console.log("sono passati 60000 millisecondi");
+//     console.log("sono passati 60000 millisecondi");
 
-});
+// });
 
 
 let tmpDirectory = path.join(__dirname, '../src/test.txt');
@@ -20,6 +20,7 @@ let tmpDirectory = path.join(__dirname, '../src/test.txt');
 if (cfg.gateway && cfg.gateway.path_resolv) {
     tmpDirectory = cfg.gateway.path_resolv;
 }
+console.log("parte il watchfile");
 fs.watchFile(tmpDirectory, async(curr : any, prev: any) => {
     //console.log("Sto ascoltando il file");
     console.log( `[${new Date().toLocaleString()}] Watching for file changes on: ${tmpDirectory}`  );
