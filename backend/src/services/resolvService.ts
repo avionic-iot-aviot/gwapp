92798911812ae8b5c5471a72545a3e6808cdd74d
Carmelo <carmelo@Mac-mini-di-Deborah.local>
2020-03-05 09:17:50 +0100

    fix sudo command

--- backend/src/services/resolvService.ts
+++ backend/src/services/resolvService.ts
@@ -195,7 +195,7 @@ export default class ResolvService {
     async replace_file(){
 
         try {
-            const { stdout, stderr } = await exec(`sudo cat ${cfg.gateway.path_temp_out} > ${cfg.gateway.path_resolv}`);
+            const { stdout, stderr } = await exec(` /bin/bash -c 'sudo cat ${cfg.gateway.path_temp_out} > ${cfg.gateway.path_resolv}'`);
             console.log('RESOLV: stdout:', stdout);
             console.log('RESOLV: stderr:', stderr);
         } catch (error) {
