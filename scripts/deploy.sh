153bf643a342a206a557beec07aced30bbe315b6
Melania Dello Spedale La Paglia <melania.dellospedalelapaglia@netsenseweb.com>
2020-03-30 10:24:44 +0200

    aggiornato script di deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,17 @@
 PROJECT_FOLDER="$gwapp"
 
-if [ ! -d PROJECT_FOLDER ] ; then
-    git clone https://github.com/avionic-iot-aviot/gwapp.git
-    
+if [ ! -d $PROJECT_FOLDER ] ; then
+echo 'NOT Exists'
 else
 echo 'Already Exists'
+pm2 stop gatewayapp
+sudo rm -rf ~/gwapp
 fi
-cd /home/pi/gwapp/backend;
+git clone -b resolv --single-branch  git://github.com/avionic-iot-aviot/gwapp.git
+#git clone https://github.com/avionic-iot-aviot/gwapp.git
+cd ~/gwapp/backend;
 npm install
 npm run be:build
-cd /home/pi/gwapp/backend
-NODE_ENV=staging pm2 start dist/main.js --name "gatewayapp" && cd /home/pi/ && pm2 startup > pm2_startup_output;
+cd ~/gwapp/backend
+NODE_ENV=staging pm2 start dist/main.js --name "gatewayapp" && cd ~/ && pm2 startup > pm2_startup_output;
 tail -n 1 pm2_startup_output > pm2_startup.sh && chmod u+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
