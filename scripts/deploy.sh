1b012faa293132b42305cd893cc3ea579f7eb885
Marco Cavalli <cmarcos46@gmail.com>
2021-06-18 18:22:22 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,3 +13,4 @@ npm install
 npm run be:build
 NODE_ENV=staging pm2 start dist/main.js --name "gatewayapp" && cd ~/ && pm2 startup > pm2_startup_output;
 tail -n 1 pm2_startup_output > pm2_startup.sh && chmod u+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
+cd ~
