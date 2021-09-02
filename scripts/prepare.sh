f827bf01fd01ec479db708c390bbace58fa371a6
Marco Cavalli <cmarcos46@gmail.com>
2021-09-02 16:48:34 +0200

    Added install of a packet needed for janus streaming

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -10,6 +10,7 @@ sudo apt -y install unzip
 sudo apt -y install ebtables
 sudo apt -y install git
 sudo apt -y install screen
+sudo apt -y install gstreamer1.0-tools
 
 # Install nvm v0.37.2 from their repo
 git clone https://github.com/nvm-sh/nvm.git .nvm
