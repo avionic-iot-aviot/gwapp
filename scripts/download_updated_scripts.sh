3d94cf222b39345092f8903255094b736ca83dd9
Marco Cavalli <cmarcos46@gmail.com>
2021-09-09 13:10:19 +0200

    Added a new script to run whenever the user wants to update the scripts to perfom updates (deploy.sh and update.sh)

---
+++ scripts/download_updated_scripts.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/deploy.sh --output deploy.sh
+curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/update.sh --output update.sh
