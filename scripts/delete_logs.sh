5954602beb016aaab0e8c8b85cb6c41e806877a5
Marco Cavalli <cmarcos46@gmail.com>
2021-09-09 13:32:12 +0200

    added correct time for delete_logs script to be run

--- scripts/delete_logs.sh
+++ scripts/delete_logs.sh
@@ -1,4 +1,8 @@
 # deletes all logs when greater than a specific size
-find ~/ -maxdepth 1 -name "*.tlog*" -size +5M -delete
-find ~/ardupilot*/logs -name "*.BIN" -size +5M -delete
-find ~/blackbox -name "*.tlog" -size +5M -delete
+#find ~/ -maxdepth 1 -name "*.tlog*" -size +5M -delete
+#find ~/ardupilot*/logs -name "*.BIN" -size +5M -delete
+#find ~/blackbox -name "*.tlog" -size +5M -delete
+
+find ~/ -maxdepth 1 -name "*.tlog*" -size +5M -exec cp /dev/null '{}' \;
+find ~/ardupilot*/logs -name "*.BIN" -size +5M -exec cp /dev/null '{}' \;
+find ~/blackbox -name "*.tlog" -size +5M -exec cp /dev/null '{}' \;
