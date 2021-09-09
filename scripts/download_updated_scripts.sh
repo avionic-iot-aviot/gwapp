10b0096a0dd96b62e717a00f7a37bfe1e622ca44
Marco Cavalli <cmarcos46@gmail.com>
2021-09-09 13:16:51 +0200

    scripts are now executable

--- scripts/download_updated_scripts.sh
+++ scripts/download_updated_scripts.sh
@@ -1,2 +1,4 @@
 curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/deploy.sh --output deploy.sh
 curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/update.sh --output update.sh
+chmod +x deploy.sh
+chmod +x update.sh
