40f11690ae841f63cd20f8cea6da9c1307c97018
Marco Cavalli <cmarcos46@gmail.com>
2021-09-09 13:32:21 +0200

    Update add_crontab_jobs.sh

--- scripts/add_crontab_jobs.sh
+++ scripts/add_crontab_jobs.sh
@@ -1,3 +1,3 @@
 crontab -r
 echo "$(echo '@reboot /home/pi/reboot.sh >> /home/pi/reboot-sh.log 2>&1' ; crontab -l)" | crontab -
-echo "$(echo 'bash /home/pi/delete_logs.sh' ; crontab -l)" | crontab -
+echo "$(echo '0 * * * * bash /home/pi/delete_logs.sh' ; crontab -l)" | crontab -
