d564c4d89d113fe2daf39ac25fa1ad3314c25702
Marco Cavalli <cmarcos46@gmail.com>
2021-09-14 17:56:58 +0200

    fixed wrong folder where it starts the screens

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -45,6 +45,7 @@ screen -S 'ros-device-streamer' -d -m python3 ros_stream.py -c configs.json
 
 
 #run handler for gstreamer
+cd /home/pi/audio-receiver/aviot-streamer-handler
 screen -S 'handler-3.1' -m -d python3 handler.py --audio-raw ../192.168.3.1.bin --janus-port 60001 --janus-ip janus --time-interval-start-stream 6 --time-interval-stop-stream 10 --check-period 1
 screen -S 'handler-3.2' -m -d python3 handler.py --audio-raw ../192.168.3.2.bin --janus-port 60002 --janus-ip janus --time-interval-start-stream 6 --time-interval-stop-stream 10 --check-period 1
 screen -S 'handler-3.3' -m -d python3 handler.py --audio-raw ../192.168.3.3.bin --janus-port 60003 --janus-ip janus --time-interval-start-stream 6 --time-interval-stop-stream 10 --check-period 1
