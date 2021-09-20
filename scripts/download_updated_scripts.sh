110524f91f42c5ca1ba6d6887caa9130ce387628
Marco Cavalli <cmarcos46@gmail.com>
2021-09-20 11:24:09 +0200

    When downloading updated scripts, it will overwrite the previous file

--- scripts/download_updated_scripts.sh
+++ scripts/download_updated_scripts.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/deploy.sh --output deploy.sh
-curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/update.sh --output update.sh
+curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/deploy.sh > deploy.sh
+curl https://raw.githubusercontent.com/avionic-iot-aviot/gwapp/master/scripts/update.sh > update.sh
 chmod +x deploy.sh
 chmod +x update.sh
